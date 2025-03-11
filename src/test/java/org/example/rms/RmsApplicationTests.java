5e35d3fc54c1137812f3e3f2b10a2b92547257e3
d4tzz <nvdatz2k4@gmail.com>
2025-03-11 17:00:29 +0700

    initial commit

---
+++ src/test/java/org/example/rms/RmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
