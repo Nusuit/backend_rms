96b2d8aa2f0c8ae80a8d7f6518e1de2176ec95a5
d4tzz <nvdatz2k4@gmail.com>
2025-03-21 19:08:34 +0700

    .

--- src/main/java/org/example/rms/RmsApplication.java
+++ src/main/java/org/example/rms/RmsApplication.java
@@ -1,13 +1,17 @@
 package org.example.rms;
 
+import org.example.rms.utils.JwtUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+
 public class RmsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RmsApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(RmsApplication.class, args);
     }
-
 }
